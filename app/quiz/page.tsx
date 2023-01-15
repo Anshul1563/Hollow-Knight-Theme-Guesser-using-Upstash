b275376008b957bbdfc2ddbb2370ea1f884b7bd1
Anshul1563 <pesanshul@gmail.com>
2023-01-15 16:08:26 +0530

    Fetching data

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,17 +1,16 @@
 import AudioInterface from "./AudioInterface";
 import "../../styles/quiz.css";
-import Themes from "./themes";
 
 
 async function FetchThemes() {
-	const res = await fetch("/api/SearchThemes");
+	const res = await fetch("https://hk-themes.vercel.app/api/SearchThemes");
 	const result = await res.json();
 	return result;
 }
 
 export default async function App() {
-	// let data = await FetchThemes();
-	let data = Themes
+	let data = await FetchThemes();
+	
 
 	return (
 		<>
--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,15 +1,14 @@
 import BarGraph from "./BarGraph";
-import Data from "../quiz/themes";
+
 
 async function FetchThemes() {
-	const res = await fetch("/api/SearchThemes",{ next: { revalidate: 60 }});
+	const res = await fetch("https://hk-themes.vercel.app/api/SearchThemes",{ next: { revalidate: 60 }});
 	const { themes } = await res.json();
 	return themes;
 }
 
 async function Results() {
-	// const Themes = await FetchThemes();
-	let Themes = Data.themes;
+	const Themes = await FetchThemes();
 
 	return (
 		<div className="h-screen w-full bg-[hsl(273,93%,17%)] flex flex-col justify-center items-center p-12 pt-6 gap-6">
