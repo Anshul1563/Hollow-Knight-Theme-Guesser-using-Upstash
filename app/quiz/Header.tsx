8ec6f702ff4d95d5c0ae98179ce258a8f829eb9e
Anshul1563 <pesanshul@gmail.com>
2023-01-16 13:06:08 +0530

    Changed header and play menu

--- app/quiz/Header.tsx
+++ app/quiz/Header.tsx
@@ -4,7 +4,7 @@ import HKlogo from "../../public/HKlogo.svg";
 
 function Header() {
 	return (
-		<div className="sticky top-0 w-full z-50 flex-col  phone:flex-row bg-[#FF9F5F] text-[hsl(273,92%,27%)] font-medium flex gap-2 phone:gap-8 p-4 px-4 phone:px-8 items-center phone:justify-between">
+		<div className="sticky top-0 w-full z-50 flex-col phone:flex-row bg-[hsl(24,100%,69%)] text-[hsl(273,92%,27%)] pt-4 phone:pb-4 font-medium flex gap-2 phone:gap-8 phone:px-8 items-center phone:justify-between">
 			<Image
 				priority={true}
 				className="filter-white"
@@ -13,10 +13,10 @@ function Header() {
 				width={100}
 				alt="HK logo"
 			></Image>
-			<div className="flex gap-8 justify-between w-full phone:w-auto phone:justify-start">
-				<Link className="flex justify-center transition-all hover:scale-110 active:scale-100 items-center" href="/results">Results</Link>
-				<Link className="flex justify-center transition-all hover:scale-110 active:scale-100 items-center" href="/quiz">Quiz</Link>
-				<Link className="flex justify-center transition-all hover:scale-110 active:scale-100 items-center" href="/">Home</Link>
+			<div className="flex justify-around text-base w-full phone:w-auto phone:justify-start">
+				<Link className="flex p-2 flex-1 phone:w[100px] md:w-[150px]  bg-[hsl(24,100%,65%)] border-x-2 border-r-0 border-[#ffffff] justify-center transition-all hover:scale-110 active:scale-100 items-center" href="/results">Results</Link>
+				<Link className="flex p-2 flex-1 phone:w[100px] md:w-[150px]  bg-[hsl(24,100%,65%)] border-x-2 border-[#ffffff] justify-center transition-all hover:scale-110 active:scale-100 items-center" href="/quiz">Quiz</Link>
+				<Link className="flex p-2 flex-1 phone:w[100px] md:w-[150px]  bg-[hsl(24,100%,65%)] border-x-2 border-l-0 border-[#ffffff] justify-center transition-all hover:scale-110 active:scale-100 items-center" href="/">Home</Link>
 			</div>
 		</div>
 	);
