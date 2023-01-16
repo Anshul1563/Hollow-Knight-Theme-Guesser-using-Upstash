603b7255ef9e4a6ac2453c68b29e657282a8ede4
Anshul1563 <pesanshul@gmail.com>
2023-01-16 15:53:02 +0530

    Results page padding

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -14,7 +14,7 @@ async function Results() {
 	return (
 		<div className="h-[150vh] w-full bg-[hsl(273,93%,17%)] flex flex-col justify-center items-center">
 			<Header />
-			<div className=" flex flex-col justify-center items-center p-12 pt-6 gap-6 flex-1 w-full ">
+			<div className=" flex flex-col justify-center items-center phone:p-12 p-2 pt-6 gap-6 flex-1 w-full ">
 				<h1 className="text-4xl p-2 rounded-md bg-[#FF9F5F] text-[hsl(273,93%,27%)] font-medium ">
 					Which are Hollow Knight&apos;s Most Recognizable Themes ?
 				</h1>
