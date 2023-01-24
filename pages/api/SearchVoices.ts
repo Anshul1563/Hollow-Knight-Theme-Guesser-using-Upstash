c6e9a5a98f2944d0e7bdc48eece4010fe5c51259
Anshul1563 <pesanshul@gmail.com>
2023-01-24 23:50:01 +0530

    Added Voices api

---
+++ pages/api/SearchVoices.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import redis from '../../lib/upstash-redis';
+import { Theme } from '../../typings';
+
+
+type Data = {
+    themes : Theme[]
+}
+
+type ErrorData = {
+    body : string
+}
+
+export default async function handler(req: NextApiRequest,
+    res: NextApiResponse<Data | ErrorData>) {
+
+    if (req.method!= "GET"){
+        res.status(405).json({body : "Method not allowed"})
+        return;
+    }
+    
+    const themeResponse =  await redis.hvals("voices")
+
+    const themes : Theme[] = themeResponse.map((theme)=>{
+        return JSON.parse(theme)
+    }).sort((a : Theme,b : Theme)=> b.name > a.name ? -1 : 1 )
+
+    res.status(200).json({themes})
+
+
+}
